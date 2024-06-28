35d93e07f138073e34a27c13d270c7d5f549f488
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-06-28 11:56:33 +0000

    code formatting

--- src/router.ts
+++ src/router.ts
@@ -1,15 +1,19 @@
-import { initTRPC } from '@trpc/server';
-import { z } from 'zod';
+import { initTRPC } from "@trpc/server";
+import { z } from "zod";
 
 const t = initTRPC.create();
 
 export const appRouter = t.router({
-  greeting: t.procedure.input(z.object({ name: z.string() })).query(({ input }) => {
-    return `Hello, ${input.name}!`;
-  }),
-  add: t.procedure.input(z.object({ a: z.number(), b: z.number() })).mutation(({ input }) => {
-    return input.a + input.b;
-  }),
+  greeting: t.procedure
+    .input(z.object({ name: z.string() }))
+    .query(({ input }) => {
+      return `Hello, ${input.name}!`;
+    }),
+  add: t.procedure
+    .input(z.object({ a: z.number(), b: z.number() }))
+    .mutation(({ input }) => {
+      return input.a + input.b;
+    }),
 });
 
 export type AppRouter = typeof appRouter;
